d0124f8db09a337f3a62adedcada2e8d60a76483
maurocristofoli <texturalnote@gmail.com>
2019-04-24 11:04:15 +0200

    Prova M

--- app/src/main/java/com/example/progettoapplicazionimobili/MainActivity.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // Testing Git Jerry PRO
+    // Testing Git Jerry GG2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
