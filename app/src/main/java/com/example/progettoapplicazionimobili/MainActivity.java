bc4fc9ae813521f0a17f0ae0e8401739275d069f
maurocristofoli <texturalnote@gmail.com>
2019-04-24 10:52:43 +0200

    Prova M

--- app/src/main/java/com/example/progettoapplicazionimobili/MainActivity.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // Testing Git
+    // Testing Git RandomM
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
