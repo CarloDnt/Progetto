8b139806384285d494126a2a2a15ce8de424e6e5
Carlo <carlodentesano@yahoo.it>
2019-04-26 11:31:45 +0200

    Login

--- app/src/main/java/com/example/progettoapplicazionimobili/ui/Home.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/Home.java
@@ -1,13 +1,16 @@
-package com.example.progettoapplicazionimobili;
+package com.example.progettoapplicazionimobili.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.progettoapplicazionimobili.R;
+
+public class Home extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.startup);
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.progettoapplicazionimobili.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.progettoapplicazionimobili.R;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username=(EditText) findViewById(R.id.etUsername);
+        password=(EditText) findViewById(R.id.etPassword);
+        login=(Button) findViewById(R.id.btLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(username.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+    private void validate(String userName,String userPassword){
+        if((userName.equals("Admin")) && (userPassword.equals("1234"))){
+            Intent intent=new Intent(MainActivity.this, Home.class);
+            startActivity(intent);
+        }
+    };
+}
