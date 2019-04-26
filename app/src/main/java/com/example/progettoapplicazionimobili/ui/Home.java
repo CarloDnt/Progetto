5c51adec1632eca088432cc5518efa204a847d41
Carlo <carlodentesano@yahoo.it>
2019-04-26 12:25:01 +0200

    Login

--- app/src/main/java/com/example/progettoapplicazionimobili/ui/Home.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/Home.java
@@ -1,16 +1,49 @@
 package com.example.progettoapplicazionimobili.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.example.progettoapplicazionimobili.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dispensa:
+                    mTextMessage.setText(R.string.dispensa);
+                    return true;
+                case R.id.navigation_calendario:
+                    mTextMessage.setText(R.string.calendario);
+                    return true;
+                case R.id.navigation_lista:
+                    mTextMessage.setText(R.string.lista);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
