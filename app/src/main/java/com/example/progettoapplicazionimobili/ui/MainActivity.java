f104d2235adb5c3addcb231c372b368e264e0bd0
Carlo <carlodentesano@yahoo.it>
2019-04-29 13:30:39 +0200

    Inserimento Database Utenti:preliminare

--- app/src/main/java/com/example/progettoapplicazionimobili/ui/MainActivity.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/MainActivity.java
@@ -2,30 +2,41 @@ package com.example.progettoapplicazionimobili.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.progettoapplicazionimobili.R;
 
-import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
     private EditText username;
     private EditText password;
+    private TextView registrati;
     private Button login;
-    private RealmUtenti realmManipulator=RealmUtenti.getRealmInstance(getApplicationContext());
+    private RealmUtenti realmManipulator;
+    private static Context appContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        realmManipulator=RealmUtenti.getRealmInstance(getApplicationContext());
         username=(EditText) findViewById(R.id.etUsername);
         password=(EditText) findViewById(R.id.etPassword);
         login=(Button) findViewById(R.id.btLogin);
-
+        registrati=(TextView) findViewById((R.id.twregistrati));
+        registrati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, Registrazione.class);
+                startActivity(intent);
+            }
+        });
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
     private void validate(String userName,String userPassword,RealmUtenti realm){
         RealmResults<UtentiApp> utente=realm.getUtentiPrecisi(userName);
         if(utente.size()!=0){
-            String password=utente.get(1).getPasswordUtente();
+            String password=utente.get(0).getPasswordUtente();
             if(userPassword.equals(password)){
                 Intent intent=new Intent(MainActivity.this, Home.class);
                 startActivity(intent);
---
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/Registrazione.java
@@ -0,0 +1,46 @@
+package com.example.progettoapplicazionimobili.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.progettoapplicazionimobili.R;
+
+public class Registrazione extends AppCompatActivity {
+    private EditText username;
+    private EditText password;
+    private EditText password2;
+    private Button registrati;
+    private RealmUtenti realmManipulator;
+    private static Context appContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrazione);
+        realmManipulator=RealmUtenti.getRealmInstance(getApplicationContext());
+        username=(EditText)findViewById(R.id.etUsername);
+        password=(EditText)findViewById(R.id.etPassword);
+        password2=(EditText)findViewById(R.id.etPassword2);
+        registrati=(Button)findViewById(R.id.btAggiungi);
+        registrati.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registraUtente(username.getText().toString(),password.getText().toString(),password2.getText().toString(),realmManipulator);
+            }
+        });
+    }
+    public void registraUtente(String username,String password,String password2,RealmUtenti realm){
+        if(password.equals(password2)){
+            UtentiApp utente=new UtentiApp(username,password);
+            realm.addOrUpdateRealmList(utente);
+            Intent intent=new Intent(Registrazione.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
