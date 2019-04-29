e754166b478f67d35598a3b2474fdc3bd671d133
Carlo <carlodentesano@yahoo.it>
2019-04-29 15:33:28 +0200

    Inizio Add Prodotti

---
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/AggiungiProdotto.java
@@ -0,0 +1,16 @@
+package com.example.progettoapplicazionimobili.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.progettoapplicazionimobili.R;
+
+public class AggiungiProdotto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aggiungi_prodotto);
+    }
+}
--- app/src/main/java/com/example/progettoapplicazionimobili/ui/HomeFragment.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,10 +14,16 @@ import androidx.fragment.app.Fragment;
 import com.example.progettoapplicazionimobili.R;
 
 public class HomeFragment extends Fragment {
-
+    private ImageView add;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        add=(ImageView)getView().findViewById(R.id.add);
     }
 }
