079c8dd18c134e1c1e95a11b72210ed37f4c1705
LucaFormentin <formentin.luca97@gmail.com>
2019-04-30 13:45:04 +0200

    Dispensa

--- app/src/main/java/com/example/progettoapplicazionimobili/ui/DispensaFragment.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/DispensaFragment.java
@@ -15,8 +15,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.progettoapplicazionimobili.R;
 
