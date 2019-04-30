7682c590dcbf57f6fbc75e46875af360cb9f6444
Carlo <carlodentesano@yahoo.it>
2019-04-30 17:08:41 +0200

    Calendario

--- app/src/main/java/com/example/progettoapplicazionimobili/ui/CalendarioFragment.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/CalendarioFragment.java
@@ -1,22 +1,33 @@
 package com.example.progettoapplicazionimobili.ui;
 
+import android.graphics.Color;
+import android.icu.text.DateFormat;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.DragEvent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.progettoapplicazionimobili.R;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+
+import java.util.Date;
+import java.util.List;
+
+import io.realm.RealmResults;
+
 
 public class CalendarioFragment extends Fragment {
 
@@ -24,10 +35,16 @@ public class CalendarioFragment extends Fragment {
     private ViewPager viewPager;
     private CustomSwipe customSwipe;
     private ProgressBar bar;
+    private RealmResults<ProdottoDisp> prodotti;
+    private CompactCalendarView customCalendar;
+    private TextView txtevento;
+    private static final String TAG = "Calendario";
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        RealmDispensa dispManager=RealmDispensa.getRealmInstance(getContext());
+        this.prodotti=dispManager.getAllProdotti();
         return inflater.inflate(R.layout.fragment_calendario,container,false);
     }
 
@@ -39,7 +56,7 @@ public class CalendarioFragment extends Fragment {
         customSwipe = new CustomSwipe(getContext());
         viewPager.setAdapter(customSwipe);
         bar=getView().findViewById(R.id.barprogress);
-        bar.setMax(3);
+        bar.setMax(customSwipe.getCount()-1);
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -61,5 +78,29 @@ public class CalendarioFragment extends Fragment {
                 return true;
             }
         });
+
+        customCalendar = (CompactCalendarView) getView().findViewById(R.id.compactcalendar_view);
+        txtevento=getView().findViewById(R.id.Evento);
+
+        for(int i=0;i<prodotti.size();i++) {
+            Long time=prodotti.get(i).getScadenza().getTime();
+            String nomeprodotto=prodotti.get(i).getNomeProdotto();
+            Event ev1 = new Event(Color.GREEN, time, nomeprodotto);
+            customCalendar.addEvent(ev1);
+        }
+        customCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                List<Event> events = customCalendar.getEvents(dateClicked);
+                if(events.size()>0) {
+                    txtevento.setText("Scade: " + events.get(0).getData().toString());
+                }
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/progettoapplicazionimobili/ui/CustomSwipe.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/CustomSwipe.java
@@ -1,34 +1,38 @@
 package com.example.progettoapplicazionimobili.ui;
 
 import android.content.Context;
-import android.os.Bundle;
+import android.graphics.BitmapFactory;
+import android.icu.text.DateFormat;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.annotation.RequiresApi;
 import androidx.viewpager.widget.PagerAdapter;
 import com.example.progettoapplicazionimobili.R;
-
 import java.util.*;
+import io.realm.RealmResults;
 
 public class CustomSwipe extends PagerAdapter {
 
-    private int[] image_resources = {R.drawable.fake1, R.drawable.fake2, R.drawable.fake3};
     private Context ctx;
     private LayoutInflater layoutInflater;
+    private RealmResults<ProdottoDisp> prodotti;
 
     public CustomSwipe(Context ctx){
         this.ctx = ctx;
+        RealmDispensa dispManager=RealmDispensa.getRealmInstance(this.ctx);
+        this.prodotti=dispManager.getAllProdotti();
     }
 
     @Override
     public int getCount() {
-        return image_resources.length;
+        if(prodotti.size()<10){
+        return prodotti.size();}else{return 10;}
     }
 
     @Override
@@ -36,6 +40,7 @@ public class CustomSwipe extends PagerAdapter {
         return (view == o);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public Object instantiateItem(ViewGroup container, int position){
 
@@ -50,13 +55,16 @@ public class CustomSwipe extends PagerAdapter {
         //DataScadenza
         TextView dataScadenza = item_view.findViewById(R.id.data_scadenza);
         //Date
-        EditText data = item_view.findViewById(R.id.et_date);
+        TextView data = item_view.findViewById(R.id.et_date);
 
-        imageView.setImageResource(image_resources[position]);
+        byte[] img=prodotti.get(position).getImg();
+        imageView.setImageBitmap(BitmapFactory.decodeByteArray(img, 0, img.length));
         nomeProdotto.setText("Prodotto: ");
-        nome.setText("...");
+        nome.setText(prodotti.get(position).getNomeProdotto());
         dataScadenza.setText("Data scadenza: ");
-        data.setText("...");
+        Date scadenza=prodotti.get(position).getScadenza();
+        String scadenzatxt= DateFormat.getDateInstance(DateFormat.LONG).format(scadenza);
+        data.setText(scadenzatxt);
         container.addView(item_view);
 
         return item_view;
