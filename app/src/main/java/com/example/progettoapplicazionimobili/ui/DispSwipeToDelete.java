67ad0aefe9d6e972ac080f446550e1ac62d9da15
Carlo <carlodentesano@yahoo.it>
2019-05-15 15:20:46 +0200

    Swipe Delete

--- app/src/main/java/com/example/progettoapplicazionimobili/ui/DispSwipeToDelete.java
+++ app/src/main/java/com/example/progettoapplicazionimobili/ui/DispSwipeToDelete.java
@@ -23,6 +23,7 @@ public class DispSwipeToDelete extends ItemTouchHelper.SimpleCallback {
         mAdapter = adapter;
         icon = ContextCompat.getDrawable(mAdapter.getContext(),
                 R.drawable.ic_close_black_24dp);
+        icon.setTint(mAdapter.getContext().getResources().getColor(R.color.primaryTextColor));
         background = new ColorDrawable(Color.RED);
     }
 
@@ -42,6 +43,43 @@ public class DispSwipeToDelete extends ItemTouchHelper.SimpleCallback {
                 dY, actionState, isCurrentlyActive);
         View itemView = viewHolder.itemView;
         int backgroundCornerOffset = 20;
+        if (dX > 0) { // Swiping to the right
+            background.setBounds(itemView.getLeft(), itemView.getTop(),
+                    itemView.getLeft() + ((int) dX) + backgroundCornerOffset,
+                    itemView.getBottom());
+
+        } else if (dX < 0) { // Swiping to the left
+            background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
+                    itemView.getTop(), itemView.getRight(), itemView.getBottom());
+        } else { // view is unSwiped
+            background.setBounds(0, 0, 0, 0);
+        }
+        background.draw(c);
+        int iconMargin = (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconTop = itemView.getTop() + (itemView.getHeight() - icon.getIntrinsicHeight()) / 2;
+        int iconBottom = iconTop + icon.getIntrinsicHeight();
+
+        if (dX > 0) { // Swiping to the right
+            int iconLeft = itemView.getLeft() + iconMargin + icon.getIntrinsicWidth();
+            int iconRight = itemView.getLeft() + iconMargin;
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getLeft(), itemView.getTop(),
+                    itemView.getLeft() + ((int) dX) + backgroundCornerOffset,
+                    itemView.getBottom());
+        } else if (dX < 0) { // Swiping to the left
+            int iconLeft = itemView.getRight() - iconMargin - icon.getIntrinsicWidth();
+            int iconRight = itemView.getRight() - iconMargin;
+            icon.setBounds(iconLeft, iconTop, iconRight, iconBottom);
+
+            background.setBounds(itemView.getRight() + ((int) dX) - backgroundCornerOffset,
+                    itemView.getTop(), itemView.getRight(), itemView.getBottom());
+        } else { // view is unSwiped
+            background.setBounds(0, 0, 0, 0);
+        }
+
+        background.draw(c);
+        icon.draw(c);
     }
 
 }
